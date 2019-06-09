bc244b1bdc6ebdee093d3618f5d0ac8dd8ff1162
Marcus Povey <marcus@marcus-povey.co.uk>
2019-06-09 14:51:23 +0100

    Actually saving app

--- app/controllers/oauthapp_controller.rb
+++ app/controllers/oauthapp_controller.rb
@@ -1,8 +1,31 @@
+require 'digest/sha1'
+
 class OauthappController < ApplicationController
     def new
     end
 
     def create
-        render plain: params[:oauthapp].inspect
+        # Generate key
+        params[:oauthapp][:userid] = 1 # Userid: TODO Get this from session
+        params[:oauthapp][:pubkey] = Digest::SHA1.hexdigest rand().to_s
+        params[:oauthapp][:secret] = Digest::SHA1.hexdigest rand().to_s
+        
+        # TODO: Check we've not duplicated key 
+
+        #render plain: params[:oauthapp].inspect
+        @Oauthapp = Oauthapp.new(oauthapp_params)
+
+        @Oauthapp.save
+        redirect_to @Oauthapp
+
     end
+
+    def show
+        @oauthapp = Oauthapp.find(params[:id])
+    end
+
+    private 
+        def oauthapp_params
+            params.require(:oauthapp).permit(:title, :pubkey, :secret)
+        end
 end
