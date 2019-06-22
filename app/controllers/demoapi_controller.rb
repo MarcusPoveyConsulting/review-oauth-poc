3828663f4b771844e265ba057446756fc41ab94b
Marcus Povey <marcus@marcus-povey.co.uk>
2019-06-22 19:43:28 +0100

    Demo user api endpoint

--- app/controllers/demoapi_controller.rb
+++ app/controllers/demoapi_controller.rb
@@ -5,13 +5,29 @@ class DemoapiController < ApplicationController
 
 
         params.permit(:access_token);
-        access_token = params[:access_token]
 
-        
+
         # Validate auth token
+        token = Oauthtoken.where(access_token: params[:access_token]).first
+        if !token?
+            raise "Token invalid"
+        end
 
+        # Todo validate expiry
 
         # If ok, then return demo user
+        user = token.user_id
+
+        if user == 1
+            userdetails = {
+                :user_id => 1,
+                :username => 'demouser',
+                :name => 'Demo User'
+            } 
+
+            render json: userdetails.to_json
+        end
+        
 
     end
 
